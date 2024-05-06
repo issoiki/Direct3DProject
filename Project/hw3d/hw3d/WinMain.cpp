461487acf1182e81272db740f4e84870b02ce4ba
Ivan <misternoob1324@gmail.com>
2024-05-06 23:18:35 +0700

    Window framework updated

--- Project/hw3d/hw3d/WinMain.cpp
+++ Project/hw3d/hw3d/WinMain.cpp
@@ -1,26 +1,6 @@
-#include <Windows.h>
+#include "Window.h"
 
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-
-
-    switch (msg) {
-    case WM_CLOSE:
-        PostQuitMessage(69);
-        break;
-    case WM_KEYDOWN:
-        if (wParam == 'F') {
-            SetWindowText(hWnd, L"Я того все рот ебал реально");
-        }
-        break;
-    case WM_KEYUP:
-        if (wParam == 'F') {
-            SetWindowText(hWnd, L"Это пздец");
-        }
-        break;
-    }
-    return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
     HINSTANCE hInstance,
@@ -28,36 +8,8 @@ int CALLBACK WinMain(
     PSTR lpCmdLine,
     int nCmdShow)
 {
+    Window wnd(800, 300, L"Выпадение кишки");
 
-    const auto pClassName = L"h3wd";
-    //register window class
-    WNDCLASSEX wc = { 0 };
-    wc.cbSize = sizeof(wc);
-    wc.style = CS_OWNDC;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = nullptr;
-    wc.hCursor = nullptr;
-    wc.hbrBackground = nullptr;
-    wc.lpszMenuName = nullptr;
-    wc.lpszClassName = pClassName;
-    RegisterClassEx(&wc);
-
-    //create window instance
-    HWND hWnd = CreateWindowEx(
-        0, pClassName,
-        L"Кишка(выпала)",
-        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-        200, 200,
-        640, 480,
-        nullptr, nullptr, hInstance, nullptr
-    );
-    //show window
-    ShowWindow(hWnd, SW_SHOW);
-
-    //message pump
 
     MSG msg;
     BOOL gResult;
@@ -69,8 +21,6 @@ int CALLBACK WinMain(
     if (gResult == -1) {
         return -1;
     }
-    else {
-        return msg.wParam;
-    }
-    return 0;
+
+    return msg.wParam;
 }
